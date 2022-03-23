2475bb195f670a613c63760e29a7cb665775f5db
April Skrine <april.skrine@gmail.com>
2022-03-23 10:32:16 -0500

    commit tree 312b0bf50155d6d352caffe6bf4dcc0c008c07f3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+  private
+
+  def record_not_found
+    render json: { error: "Article not found" }, status: :not_found
+  end  
+
 end
